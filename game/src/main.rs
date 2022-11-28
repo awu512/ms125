809eb0424c3710ddda463a7bd149e146a1952f86
aidan <aid1@bamba.us>
2022-11-27 19:42:11 -0800

    change key system, buggy movement added

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,20 +1,23 @@
-use std::ops::RangeBounds;
+use std::ops::Index;
 use std::rc::Rc;
-use winit;
+use engine::eng::{WIDTH, HEIGHT};
+use winit::event::VirtualKeyCode;
 
-use engine;
-use engine::animations::AnimationSet;
-use engine::sprite::{Action, Character, Sprite};
+use engine::animations::{AnimationSet, PSZ};
+use engine::sprite::{Action, Sprite};
 use engine::tiles::*;
 use engine::types::*;
 
-pub const WIDTH: usize = 320;
-pub const HEIGHT: usize = 320;
+const SSZ: Vec2i = Vec2i { x: WIDTH as i32, y: HEIGHT as i32}; // SCREEN SIZE
+const TSZ: i32 = 16; // TILE SIZE
 
-const PSZ: Vec2i = Vec2i { x: 20, y: 32 };
-const PPOS: Vec2i = Vec2i { x: (WIDTH / 2) as i32 + (PSZ.x / 2), y: (HEIGHT / 2) as i32 + (PSZ.y / 2) };
+const PPOS: Vec2i = Vec2i { x: (SSZ.x / 2) - (PSZ.x / 2), y: (SSZ.y / 2) - (PSZ.y / 2) };
 
-const TILE_SZ: i32 = 16;
+const DOWN: usize = 0;
+const UP: usize = 1;
+const LEFT: usize = 2;
+const RIGHT: usize = 3;
+const SPACE: usize = 4;
 
 struct Assets {
     spritesheet: Rc<Image>,
@@ -24,20 +27,45 @@ struct Assets {
 
 struct State {
     map: Tilemap,
-    facing: Vec<bool>, // up/down, right/left
+
+    p_anims: AnimationSet,
+    p_sprite: Sprite,
+
+    tapc: [u8; 4],
+    movec: u8,
+    move_lock: bool,
+
+    dir: usize,
     pos: Pos,
 }
 
 impl State {
     pub fn new(map: Tilemap) -> Self {
-        let facing = vec![true, true];
-        let pos = Pos { x: 10., y: 10. }; // REPLACE
+        let pos = Pos { x: 10., y: 10. }; // TODO
+
+        let p_anims = AnimationSet::new(1);
+        let p_sprite = Sprite {
+            animation_state: p_anims.play_animation(Action::StandD),
+            shape: Rect {
+                pos: Vec2i { x: 20, y: 20 },
+                sz: PSZ,
+            },
+        };
 
         Self {
             map,
-            facing: vec![true, true],
+            p_anims,
+            p_sprite,
+            tapc: [0; 4],
+            movec: 0,
+            move_lock: false,
+            dir: 0,
             pos
         }
+    }
+
+    fn anim(&mut self, act: Action) {
+        self.p_sprite.animation_state = self.p_anims.play_animation(act);
     }
 }
 
@@ -47,45 +75,142 @@ fn main() {
     engine::eng::go::<Game>();
 }
 
-// [Up, Left, Right, Down]
+// [Down, Up, Left, Right, Space]
 fn update_state(s: &mut State, now_keys: &[bool], prev_keys: &[bool]) {
 
-    if now_keys[0] // UP
-    {
-        s.map.translate_y(-1);
-        s.facing[0] = true;
+    // GESTURE: RELEASE
+    if !now_keys[DOWN] && prev_keys[DOWN] && s.dir == DOWN {
+        s.move_lock = false;
+    }
+    if !now_keys[UP] && prev_keys[UP] && s.dir == UP {
+        s.move_lock = false;
+    }
+    if !now_keys[LEFT] && prev_keys[LEFT] && s.dir == LEFT {
+        s.move_lock = false;
+    }
+    if !now_keys[RIGHT] && prev_keys[RIGHT] && s.dir == RIGHT {
+        s.move_lock = false;
     }
 
-    if now_keys[1] // LEFT
-    {
-        s.map.translate_x(-1);
-        s.facing[1] = false;
+    if s.movec == 0 {
+
+        // GESTURE: TAP
+        if !now_keys[DOWN] && s.tapc[DOWN] > 0 {
+            s.anim(Action::StandD);
+        }
+        if !now_keys[UP] && s.tapc[UP] > 0 {
+            s.anim(Action::StandU);
+        }
+        if !now_keys[LEFT] && s.tapc[LEFT] > 0 {
+            s.anim(Action::StandL);
+        }
+        if !now_keys[RIGHT] && s.tapc[RIGHT] > 0 {
+            s.anim(Action::StandR);
+        }
+
+        // GESTURE: START
+        if now_keys[DOWN] && !prev_keys[DOWN] {
+            s.dir = DOWN;
+            s.tapc[DOWN] = 8;
+        }
+        if now_keys[UP] && !prev_keys[UP] {
+            s.dir = UP;
+            s.tapc[UP] = 8;
+        }
+        if now_keys[LEFT] && !prev_keys[LEFT] {
+            s.dir = LEFT;
+            s.tapc[LEFT] = 8;
+        }
+        if now_keys[RIGHT] && !prev_keys[RIGHT] {
+            s.dir = RIGHT;
+            s.tapc[RIGHT] = 8;
+        }
+
+        // GESTURE: NEW HOLD
+        if !s.move_lock {
+            if now_keys[DOWN] && s.tapc[DOWN] == 0 {
+                s.anim(Action::WalkD);
+                if s.movec == 0 { s.movec = 32; }
+            }
+            if now_keys[UP] && s.tapc[UP] == 0 {
+                s.anim(Action::WalkU);
+                if s.movec == 0 { s.movec = 32; }
+            }
+            if now_keys[LEFT] && s.tapc[LEFT] == 0 {
+                s.anim(Action::WalkL);
+                if s.movec == 0 { s.movec = 32; }
+            }
+            if now_keys[RIGHT] && s.tapc[RIGHT] == 0 {
+                s.anim(Action::WalkR);
+                if s.movec == 0 { s.movec = 32; }
+            }
+        }
     }
 
-    if now_keys[2] // RIGHT
-    {
-        s.map.translate_x(1);
-        s.facing[1] = true;
+    // SPACE START
+
+
+    // HANDLE MOVEMENT
+    if s.movec > 0 { 
+        s.movec -= 1; 
+
+        if s.movec % 2 == 1 {
+            match s.dir {
+                DOWN => s.map.translate_y(-1),
+                UP => s.map.translate_y(1),
+                LEFT => s.map.translate_x(1),
+                RIGHT => s.map.translate_x(-1),
+                _ => ()
+            }
+        }
+
+        if s.movec == 0 {
+            if s.move_lock {
+                s.movec = 32;
+            } else {
+                match s.dir {
+                    DOWN => s.anim(Action::StandD),
+                    UP => s.anim(Action::StandU),
+                    LEFT => s.anim(Action::StandL),
+                    RIGHT => s.anim(Action::StandR),
+                    _ => ()
+                }
+            }
+        }
     }
 
-    if now_keys[3] // DOWN
-    {
-        s.map.translate_y(1);
-        s.facing[0] = false;
+    // DECREASE TAP COUNTERS
+    for i in 0..4 {
+        if s.tapc[i] > 0 { s.tapc[i] -= 1; }
     }
+
+    // if now_keys[0] // DOWN
+    // {
+    //     s.map.translate_y(-1);
+    //     s.anim(Action::WalkD);
+    // }
+
+    // if now_keys[1] // UP
+    // {
+    //     s.map.translate_y(1);
+    // }
+
+    // if now_keys[2] // LEFT
+    // {
+    //     s.map.translate_x(1);
+    // }
+
+    // if now_keys[3] // RIGHT
+    // {
+    //     s.map.translate_x(-1);
+    // }
 }
 
 fn render_player(state: &mut State, assets: &mut Assets, fb2d: &mut Image) {
-    let temp = Rect {
-        pos: Vec2i { x: 10, y: 10 },
-        sz: PSZ,
-    };
-
     fb2d.bitblt(
         &assets.spritesheet,
-        temp,
-        Vec2i { x: 800, y: 800 },
-        false,
+        state.p_sprite.play_animation(&20), // TODO: investigate speedup
+        PPOS,
     );
 }
 
@@ -97,7 +222,7 @@ impl engine::eng::Game for Game {
             "game/content/tilesheet.png",
         )));
         let spritesheet = Rc::new(Image::from_file(std::path::Path::new(
-            "game/content/spritesheet.png",
+            "game/content/sp01ash.png",
         )));
         let numsheet = Rc::new(Image::from_file(std::path::Path::new(
             "game/content/numsheet.png",
@@ -120,8 +245,9 @@ impl engine::eng::Game for Game {
             ],
             tilesheet,
         ));
+
         let map = Tilemap::new(
-            Vec2i { x: -160, y: -160 },
+            Vec2i { x: -160, y: -160 }, // TODO: by map/screen width
             (40, 40),
             tileset,
             (0_usize..1600).map(|x| (x + ((x / 40 + 1) % 2)) % 2).collect::<Vec<usize>>(),
@@ -138,27 +264,32 @@ impl engine::eng::Game for Game {
     }
 
     fn update(state: &mut State, _assets: &mut Assets, now_keys: &[bool], prev_keys: &[bool]) {
-        use winit::event::VirtualKeyCode;
 
-        let now_keys = vec![
-            now_keys[VirtualKeyCode::Up as usize],
-            now_keys[VirtualKeyCode::Left as usize],
-            now_keys[VirtualKeyCode::Right as usize],
-            now_keys[VirtualKeyCode::Down as usize],
-        ];
+        // let now_keys = vec![
+        //     now_keys[VirtualKeyCode::Down as usize],
+        //     now_keys[VirtualKeyCode::Up as usize],
+        //     now_keys[VirtualKeyCode::Left as usize],
+        //     now_keys[VirtualKeyCode::Right as usize],
+        // ];
 
-        let prev_keys = vec![
-            prev_keys[VirtualKeyCode::Up as usize],
-            prev_keys[VirtualKeyCode::Left as usize],
-            prev_keys[VirtualKeyCode::Right as usize],
-            prev_keys[VirtualKeyCode::Down as usize],
-        ];
+        // let prev_keys = vec![
+        //     prev_keys[VirtualKeyCode::Down as usize],
+        //     prev_keys[VirtualKeyCode::Up as usize],
+        //     prev_keys[VirtualKeyCode::Left as usize],
+        //     prev_keys[VirtualKeyCode::Right as usize],
+        // ];
 
-        update_state(state, &now_keys, &prev_keys);
+        // let prev2_keys = vec![
+        //     prev2_keys[VirtualKeyCode::Down as usize],
+        //     prev2_keys[VirtualKeyCode::Up as usize],
+        //     prev2_keys[VirtualKeyCode::Left as usize],
+        //     prev2_keys[VirtualKeyCode::Right as usize],
+        // ];
+
+        update_state(state, now_keys, prev_keys);
     }
 
     fn render(state: &mut State, assets: &mut Assets, fb2d: &mut Image) {
-        render_player(state, assets, fb2d);
         state.map.draw(fb2d);
         render_player(state, assets, fb2d);
     }
