36552aab7d1c8781ef8f6c5fbd53b6bf8f42571b
aidan <aid1@bamba.us>
2022-12-02 23:56:00 -0800

    base implemenation

---
+++ engine/src/npc.rs
@@ -0,0 +1,79 @@
+use std::collections::HashMap;
+
+use crate::types::{DOWN, UP, LEFT, RIGHT, PSZ};
+use crate::types::{Image, Rect, Vec2i};
+
+pub struct NPC {
+    row: i32,
+    cur_dir: i32,
+    def_dir: i32,
+    pos: Vec2i
+}
+
+impl NPC {
+    pub fn new(row: i32, dir: i32, pos: Vec2i) -> Self {
+        Self { 
+            row,
+            cur_dir: dir,
+            def_dir: dir,
+            pos 
+        }
+    }
+
+    pub fn turn_to_face(&mut self, dir: usize) {
+        self.cur_dir = match dir {
+            DOWN => UP,
+            UP => DOWN,
+            LEFT => RIGHT,
+            RIGHT => LEFT,
+            _ => panic!("Invalid direction")
+        } as i32;
+    }
+
+    pub fn reset_dir(&mut self) {
+        self.cur_dir = self.def_dir;
+    }
+}
+
+pub struct NPCSet{
+    image: Image,
+    dict: HashMap<Vec2i, NPC>
+}
+
+impl NPCSet {
+    pub fn new(path: &str, npcs: Vec<NPC>) -> Self {
+        let mut dict: HashMap<Vec2i, NPC> = HashMap::new();
+        for npc in npcs {
+            dict.insert(npc.pos, npc);
+        }
+        Self {
+            image: Image::from_file(std::path::Path::new(path)),
+            dict,
+        }
+    }
+
+    pub fn draw(&self, fb2d: &mut Image, ppos: Vec2i, movec: u8, dir: usize) {
+        let sub_pos = match dir {
+            DOWN => Vec2i { x: 0, y: (movec as i32 / 2) },
+            UP => Vec2i { x: 0, y: -(movec as i32 / 2) },
+            LEFT => Vec2i { x: -(movec as i32 / 2), y: 0 },
+            RIGHT => Vec2i { x: (movec as i32 / 2), y: 0 },
+            _ => panic!("Invalid direction")
+        };
+
+        for npc in self.dict.values() {
+            fb2d.bitblt(
+                &self.image, 
+                Rect { pos: Vec2i { x: 16 * npc.cur_dir, y: 16 * npc.row }, sz: PSZ }, 
+                sub_pos + Vec2i { 
+                    x: 16 * (5 + npc.pos.x - ppos.x), 
+                    y: 16 * (5 + npc.pos.y - ppos.y)
+                }
+            );
+        }
+    }
+
+    pub fn at(&mut self, pos: Vec2i) -> Option<&mut NPC> {
+        self.dict.get_mut(&pos)
+    }
+}
